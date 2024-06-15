b1e8bd83ff6ef3abf71ef8b79ed219133f50be94
ronetel <144216054+ronetel@users.noreply.github.com>
2024-06-15 13:22:19 +0300

    Обновленная библиотека карточек

---
+++ CardsLIB/Info_card_dark.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CardsLIB
+{
+    /// <summary>
+    /// Логика взаимодействия для Change_specialist_dark.xaml
+    /// </summary>
+    public partial class Info_card_dark : UserControl
+    {
+        public Info_card_dark()
+        {
+            InitializeComponent();
+        }
+    }
+}
